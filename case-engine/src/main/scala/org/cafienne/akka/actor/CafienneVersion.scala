be3eb7cea14d33500a2fd97725392bbfe42a7d71
Thijs Petter <thijspetter@gmail.com>
2020-02-27 19:55:13 +0100

    Fix for invalid CafienneVersion comparison
    Value.convert results in different ValueMap than JSONReader.parse, causing too many "EngineVersionChanged" events

--- case-engine/src/main/scala/org/cafienne/akka/actor/CafienneVersion.scala
+++ case-engine/src/main/scala/org/cafienne/akka/actor/CafienneVersion.scala
@@ -1,9 +1,9 @@
 package org.cafienne.akka.actor
 
-import org.cafienne.cmmn.instance.casefile.{Value, ValueMap}
-import scala.collection.JavaConverters._
+import org.cafienne.cmmn.akka.BuildInfo
+import org.cafienne.cmmn.instance.casefile.{JSONReader, ValueMap}
 
-class CafienneVersion(val json: ValueMap = Value.convert(org.cafienne.cmmn.akka.BuildInfo.toMap.asJava).asInstanceOf[ValueMap]) {
+class CafienneVersion(val json: ValueMap = JSONReader.parse(BuildInfo.toJson).asInstanceOf[ValueMap]) {
   /**
     * Returns true if the two versions differ, false if they are the same.
     * @param otherVersionInstance
@@ -13,4 +13,5 @@ class CafienneVersion(val json: ValueMap = Value.convert(org.cafienne.cmmn.akka.
     !json.equals(otherVersionInstance.json)
   }
 
+  override def toString: String = json.toString
 }
