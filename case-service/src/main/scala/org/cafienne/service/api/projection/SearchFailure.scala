3122cea2554af3185c0785ade2c627dcb92cf741
Thijs Petter <thijspetter@gmail.com>
2020-05-29 15:39:26 +0200

    Search Failures now have proper error message. Fix up somewhere?

--- case-service/src/main/scala/org/cafienne/service/api/projection/SearchFailure.scala
+++ case-service/src/main/scala/org/cafienne/service/api/projection/SearchFailure.scala
@@ -2,6 +2,6 @@ package org.cafienne.service.api.projection
 
 class SearchFailure(msg: String) extends RuntimeException(msg)
 
-case class CaseSearchFailure(caseId: String) extends SearchFailure(s"A case with id '$caseId' cannot be not found.")
-case class TaskSearchFailure(taskId: String) extends SearchFailure(s"A task with id '$taskId' cannot be not found.")
-case class UserSearchFailure(userId: String) extends SearchFailure(s"A user with id '$userId' cannot be not found.")
+case class CaseSearchFailure(caseId: String) extends SearchFailure(s"A case with id '$caseId' cannot be found.")
+case class TaskSearchFailure(taskId: String) extends SearchFailure(s"A task with id '$taskId' cannot be found.")
+case class UserSearchFailure(userId: String) extends SearchFailure(s"A user with id '$userId' cannot be found.")
