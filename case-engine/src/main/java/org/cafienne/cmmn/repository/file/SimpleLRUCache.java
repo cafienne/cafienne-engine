36ab09e820c9e2c0f16d130c30f7ee5d51cbf43e
Thijs Petter <thijspetter@gmail.com>
2020-09-10 19:22:22 +0200

    Generalize SimpleLRUCache
    To make it reusable in other scenarios than case model caching

--- case-engine/src/main/java/org/cafienne/cmmn/repository/file/SimpleLRUCache.java
+++ case-engine/src/main/java/org/cafienne/cmmn/repository/file/SimpleLRUCache.java
@@ -1,7 +1,5 @@
 package org.cafienne.cmmn.repository.file;
 
-import com.typesafe.config.Config;
-import org.cafienne.akka.actor.CaseSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,14 +10,21 @@ import java.util.Map;
  * Too simple SimpleLRUCache, that takes it's max size from a Config object, defaulting to 100.
  */
 public class SimpleLRUCache<K, T> extends LinkedHashMap<K, T> {
-    private final static Logger logger = LoggerFactory.getLogger(FileBasedDefinitionProvider.class);
-    private int maxSize = 100;
+    private final static Logger logger = LoggerFactory.getLogger(SimpleLRUCache.class);
+    private final int maxSize;
 
-    public SimpleLRUCache() {
-        super(16, 0.75f, true);
-        Config config = CaseSystem.config().repository().config();
-        if (config.hasPath("cache.size")) {
-            maxSize = config.getInt("cache.size");
+    public SimpleLRUCache(int maxSize) {
+        super(maxSize >= 0 ? maxSize : 0, 0.75f, true);
+        this.maxSize = maxSize;
+    }
+
+    @Override
+    public T put(K key, T value) {
+        // Overriding this to avoid unnecessary removing eldest entry invocations
+        if (maxSize >= 0) {
+            return super.put(key, value);
+        } else {
+            return null;
         }
     }
 
@@ -28,7 +33,7 @@ public class SimpleLRUCache<K, T> extends LinkedHashMap<K, T> {
         boolean remove = size() > maxSize;
         if (remove) {
             // Printing that we remove from the cache, just to inform that the cache size may not be large enough.
-            logger.debug("Removing definitions document " + eldest.getKey() + " from the cache, since max cache size " + maxSize + " has been reached.");
+            logger.debug("Removing " + eldest.getClass().getSimpleName() + " " + eldest.getKey() + " from the cache, since max cache size " + maxSize + " has been reached.");
         }
         return remove;
     }
