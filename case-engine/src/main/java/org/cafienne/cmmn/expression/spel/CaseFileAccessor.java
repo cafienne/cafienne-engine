93022006279b620a0171a585fe7a3e3d01a0e36d
Thijs Petter <thijspetter@gmail.com>
2020-10-02 12:08:58 +0200

    Support for Map and List in SPEL expressions
    This enables bringing singular task output parameter properties into a case file item as a merge.
    E.g., parameter Response has a property Message and others, you only want to update value of Message into the case file item property 'text', then the expression map("text", Response.Message) will do the job.
    
    Also
    - code refactoring to reduce code and to go to more explicit Expression context objects.
    - debug messages for use of deprecated fields in expressions

--- case-engine/src/main/java/org/cafienne/cmmn/expression/spel/CaseFileAccessor.java
+++ case-engine/src/main/java/org/cafienne/cmmn/expression/spel/CaseFileAccessor.java
@@ -61,29 +61,3 @@ class CaseFileAccessor implements PropertyAccessor {
     }
 }
 
-/**
- * Simple wrapper around value that extends SPEL's TypedValue, so that we can return the proper value from getValue()
- *
- */
-class TypedValueWrapper extends TypedValue {
-
-    private final Value<?> value;
-
-    TypedValueWrapper(Value<?> value) {
-        super(value);
-        this.value = value;
-    }
-
-    @Override
-    public Object getValue() {
-        if (value == null) {
-            return null;
-        } else if (value.isPrimitive()) {
-            return value.getValue();
-        } else {
-            return value;
-        }
-    }
-
-}
-
