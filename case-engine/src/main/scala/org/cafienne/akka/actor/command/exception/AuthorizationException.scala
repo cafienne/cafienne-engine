cafienne/cafienne-engine

commit 8b644a53f4129fc6b047fbc3e9b4c808dd0f36df

Replace SecurityException with explicit AuthorizationException
Giving more control over the flow of handling authorizations

case-engine/src/main/scala/org/cafienne/akka/actor/command/exception/AuthorizationException.scala
@@ -1,0 +1,3 @@
+package org.cafienne.akka.actor.command.exception
+
+case class AuthorizationException(message: String) extends InvalidCommandException(message)

case-service/src/main/scala/org/cafienne/identity/IdentityCache.scala
@@ -1,6 +1,7 @@
 package org.cafienne.identity
 
 import com.typesafe.scalalogging.LazyLogging
+import org.cafienne.akka.actor.command.exception.AuthorizationException
 import org.cafienne.akka.actor.command.response.ActorLastModified
 import org.cafienne.akka.actor.identity.{PlatformUser, TenantUser}
 import org.cafienne.service.api.projection.query.UserQueries
@@ -56,7 +57,7 @@ class IdentityCache(userQueries: UserQueries)(implicit val ec: ExecutionContext)
     userQueries.getPlatformUser(userId).map(u => {
       if (u.users.isEmpty && !u.isPlatformOwner) {
         logger.info("User " + userId + " has a valid token, but is not registered in the case system")
-        throw new SecurityException("User " + userId + " is not registered in the case system")
+        throw AuthorizationException("User " + userId + " is not registered in the case system")
       }
       cache.put(userId, u)
       u

case-engine/src/main/java/org/cafienne/akka/actor/MessageHandler.java
@@ -2,11 +2,11 @@ package org.cafienne.akka.actor;
 
 import akka.actor.ActorRef;
 import org.cafienne.akka.actor.command.ModelCommand;
-import org.cafienne.akka.actor.command.exception.InvalidCommandException;
+import org.cafienne.akka.actor.command.exception.AuthorizationException;
+import org.cafienne.akka.actor.event.DebugEvent;
 import org.cafienne.akka.actor.event.EngineVersionChanged;
 import org.cafienne.akka.actor.event.ModelEvent;
 import org.cafienne.akka.actor.identity.TenantUser;
-import org.cafienne.akka.actor.event.DebugEvent;
 import org.cafienne.cmmn.instance.casefile.Value;
 import org.cafienne.cmmn.instance.debug.DebugExceptionAppender;
 import org.cafienne.cmmn.instance.debug.DebugJsonAppender;
@@ -42,6 +42,8 @@ public abstract class MessageHandler<M, C extends ModelCommand, E extends ModelE
      */
     protected final M msg;
 
+    protected final TenantUser user;
+
     private final static Logger logger = LoggerFactory.getLogger(MessageHandler.class);
 
     private final static int avgNumEvents = 30;
@@ -56,6 +58,7 @@ public abstract class MessageHandler<M, C extends ModelCommand, E extends ModelE
         // Take a copy of the sender at the moment we start handling the message (for lifecycle safety).
         this.sender = actor.sender();
         this.msg = msg;
+        this.user = user;
     }
 
     /**
@@ -64,7 +67,9 @@ public abstract class MessageHandler<M, C extends ModelCommand, E extends ModelE
      *
      * @return
      */
-    abstract protected InvalidCommandException runSecurityChecks();
+    protected AuthorizationException runSecurityChecks() {
+        return null;
+    }
 
     protected void checkEngineVersion() {
         // First check whether the engine version has changed or not; this may lead to an EngineVersionChanged event

case-engine/src/main/java/org/cafienne/akka/actor/ModelActor.java
@@ -7,6 +7,7 @@ import akka.persistence.RecoveryCompleted;
 import akka.persistence.SnapshotMetadata;
 import org.cafienne.akka.actor.command.BootstrapCommand;
 import org.cafienne.akka.actor.command.ModelCommand;
+import org.cafienne.akka.actor.command.exception.AuthorizationException;
 import org.cafienne.akka.actor.command.exception.InvalidCommandException;
 import org.cafienne.akka.actor.command.response.CommandFailure;
 import org.cafienne.akka.actor.command.response.CommandFailureListener;
@@ -300,7 +301,7 @@ public abstract class ModelActor<C extends ModelCommand, E extends ModelEvent> e
     private void runHandler(MessageHandler handler) {
         this.currentMessageHandler = handler;
 
-        InvalidCommandException securityIssue = this.currentMessageHandler.runSecurityChecks();
+        AuthorizationException securityIssue = this.currentMessageHandler.runSecurityChecks();
         if (securityIssue == null) {
             // Only process if we did not find any security issues.
             this.currentMessageHandler.process();

case-engine/src/main/java/org/cafienne/akka/actor/command/ModelCommand.java
@@ -32,16 +32,14 @@ public abstract class ModelCommand<T extends ModelActor> implements AkkaSerializ
 
     protected ModelCommand(TenantUser tenantUser, String actorId) {
         this.msgId = new Guid().toString();
-        if (tenantUser == null) {
-            throw new SecurityException("TenantUser cannot be null");
+        if (tenantUser == null || tenantUser.id() == null || tenantUser.id().trim().isEmpty()) {
+            throw new InvalidCommandException("Tenant user cannot be null");
         }
-
-        // TTR: must be validated before command is sent
         if (tenantUser.tenant() == null || tenantUser.tenant().isEmpty()) {
-            throw new SecurityException("Tenant information is missing for the "+this.getClass().getSimpleName()+" command");
+            throw new InvalidCommandException("Tenant information is missing for the "+this.getClass().getSimpleName()+" command");
         }
         if (actorId == null) {
-            throw new SecurityException("Actor id cannot be null");
+            throw new InvalidCommandException("Actor id cannot be null");
         }
         this.user = tenantUser;
         this.actorId = actorId;

case-engine/src/main/java/org/cafienne/akka/actor/command/exception/MissingTenantException.java
@@ -6,9 +6,8 @@ import org.cafienne.akka.actor.command.ModelCommand;
  * Every {@link ModelCommand} must have a tenant set.
  * If this is missing, then the command cannot be handled, and this exception is thrown.
  */
-public class MissingTenantException extends InvalidCommandException {
+public class MissingTenantException extends AuthorizationException {
     public MissingTenantException(String msg) {
         super(msg);
     }
-
 }

case-engine/src/main/java/org/cafienne/akka/actor/command/response/SecurityFailure.java
@@ -8,6 +8,7 @@
 package org.cafienne.akka.actor.command.response;
 
 import org.cafienne.akka.actor.command.ModelCommand;
+import org.cafienne.akka.actor.command.exception.AuthorizationException;
 import org.cafienne.akka.actor.serialization.Manifest;
 import org.cafienne.cmmn.instance.casefile.ValueMap;
 
@@ -23,7 +24,7 @@ public class SecurityFailure extends CommandFailure {
      * @param command
      * @param failure The reason why the command failed
      */
-    public SecurityFailure(ModelCommand command, Exception failure) {
+    public SecurityFailure(ModelCommand command, AuthorizationException failure) {
         super(command, failure);
     }
 

case-engine/src/main/java/org/cafienne/akka/actor/handler/AkkaSystemMessageHandler.java
@@ -3,22 +3,13 @@ package org.cafienne.akka.actor.handler;
 import org.cafienne.akka.actor.MessageHandler;
 import org.cafienne.akka.actor.ModelActor;
 import org.cafienne.akka.actor.command.ModelCommand;
-import org.cafienne.akka.actor.command.exception.InvalidCommandException;
 import org.cafienne.akka.actor.event.ModelEvent;
 import org.cafienne.akka.actor.identity.TenantUser;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class AkkaSystemMessageHandler<C extends ModelCommand, E extends ModelEvent, A extends ModelActor<C, E>> extends MessageHandler<Object, C, E, A> {
-    private final static Logger logger = LoggerFactory.getLogger(AkkaSystemMessageHandler.class);
 
     public AkkaSystemMessageHandler(A actor, Object msg) {
         super(actor, msg, TenantUser.NONE());
-    }
-
-    @Override
-    final protected InvalidCommandException runSecurityChecks() {
-        return null;
     }
 
     protected void process() {

commit b383e163aad69eb0c186b8eb00c47254a3d0383d

Respond proper status codes on authorization failures
Fixes #147

Resolves issue caused by commit
https://github.com/cafienne/cafienne-engine/commit/8b644a53f4129fc6b047fbc3e9b4c808dd0f36df
The commit made the try/catch SecurityException catch much less exceptions.
Especially the new AuthorizationExceptions were now handled as 500 Internal Server Errors, instead of returning 401.

Introduced explicit AuthenticationException (next to existing AuthorizationException)
Also AuthorizationException now extends plain RuntimeException to have be able to better distinguish

Also changed handling the exceptions a bit

case-engine/src/main/scala/org/cafienne/akka/actor/command/exception/AuthorizationException.scala
@@ -1,3 +1,3 @@
 package org.cafienne.akka.actor.command.exception
 
-case class AuthorizationException(message: String) extends InvalidCommandException(message)
+case class AuthorizationException(message: String) extends RuntimeException(message)

case-service/src/main/scala/org/cafienne/identity/IdentityCache.scala
@@ -41,14 +41,11 @@ class IdentityCache(userQueries: UserQueries)(implicit val ec: ExecutionContext)
     cache.get(userId) match {
       case user: PlatformUser => Future(user)
       case null => {
-//        println("User " + userId +" it not in cache. fetching it")
         userQueries.getPlatformUser(userId).map(u => {
-
           if (u.users.isEmpty && !u.isPlatformOwner) {
             logger.info("User " + userId + " has a valid token, but is not registered in the case system")
             throw AuthorizationException("User " + userId + " is not registered in the case system")
           }
-//          println("Trying to put user " + userId +" into the cache")
           cache.put(userId, u)
           u
         })

case-service/src/main/scala/org/cafienne/infrastructure/akka/http/authentication/AuthenticationDirectives.scala
@@ -2,11 +2,11 @@ package org.cafienne.infrastructure.akka.http.authentication
 
 import akka.http.scaladsl.server.directives.Credentials
 import akka.http.scaladsl.server.{Directive1, Directives}
-import org.cafienne.infrastructure.Configured
 import com.nimbusds.jose.jwk.source.JWKSource
 import com.nimbusds.jose.proc.SecurityContext
 import org.cafienne.akka.actor.identity.PlatformUser
 import org.cafienne.identity.IdentityProvider
+import org.cafienne.infrastructure.Configured
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -47,7 +47,7 @@ trait AuthenticationDirectives extends Configured {
           cachedUser <- userCache.getUser(usrCtx.subject.value, tlm)
         } yield Some(cachedUser)
       }
-      case _ => Future.successful(None)
+      case Credentials.Missing => Future.failed(MissingTokenException)
     }
   }
 

case-service/src/main/scala/org/cafienne/infrastructure/akka/http/authentication/AuthenticationException.scala
@@ -1,0 +1,8 @@
+package org.cafienne.infrastructure.akka.http.authentication
+
+class AuthenticationException(msg: String) extends RuntimeException(msg) {
+  def this(msg: String, cause: Throwable) {
+    this(msg)
+    initCause(cause)
+  }
+}

case-service/src/main/scala/org/cafienne/infrastructure/akka/http/authentication/TokenVerificationException.scala
@@ -1,6 +1,6 @@
 package org.cafienne.infrastructure.akka.http.authentication
 
-class TokenVerificationException(val msg: String) extends SecurityException(msg) {
+class TokenVerificationException(val msg: String) extends AuthenticationException(msg) {
   def this(msg: String, cause: Throwable) {
     this(msg)
     initCause(cause)
@@ -17,8 +17,7 @@ object TokenVerificationException {
     Some((e.getMessage, Option(e.getCause)))
 }
 
-class MissingTokenException() extends TokenVerificationException("Authorization token is missing")
-class InvalidJWTException() extends TokenVerificationException("Authorization token does not match JWT standard")
+object MissingTokenException extends TokenVerificationException("Authorization token is missing")
 class MissingClaimsException(msg: String) extends TokenVerificationException(msg)
 class InvalidIssuerException(msg: String) extends TokenVerificationException(msg)
 class CannotReachIDPException(msg: String, cause: Throwable) extends TokenVerificationException(msg, cause)

case-service/src/main/scala/org/cafienne/infrastructure/akka/http/authentication/TokenVerifier.scala
@@ -51,7 +51,7 @@ class JwtTokenVerifier(keySource: JWKSource[SecurityContext], issuer: String)(im
     import scala.collection.JavaConverters._
     var claimsSet: Option[JWTClaimsSet] = None
     if (token.isEmpty) {
-      throw new MissingTokenException
+      throw MissingTokenException
     }
     try {
       //noinspection ScalaStyle
@@ -103,7 +103,7 @@ class JwtTokenVerifier(keySource: JWKSource[SecurityContext], issuer: String)(im
           }
           case e: Exception => {
             logger.error("Unexpected or unforeseen exception during token verification; throwing it further", e)
-            throw new Exception("Token verification failure of type " + e.getClass.getSimpleName, e)
+            throw new TokenVerificationException("Token verification failure of type " + e.getClass.getSimpleName, e)
           }
         }
       }

case-service/src/main/scala/org/cafienne/infrastructure/akka/http/route/AuthenticatedRoute.scala
@@ -8,9 +8,10 @@ import akka.http.scaladsl.server.{Directives, ExceptionHandler, Route}
 import com.nimbusds.jose.jwk.source.{JWKSource, RemoteJWKSet}
 import com.nimbusds.jose.proc.SecurityContext
 import org.cafienne.akka.actor.CaseSystem
+import org.cafienne.akka.actor.command.exception.{AuthorizationException, InvalidCommandException}
 import org.cafienne.akka.actor.identity.PlatformUser
 import org.cafienne.identity.IdentityProvider
-import org.cafienne.infrastructure.akka.http.authentication.{AuthenticationDirectives, CannotReachIDPException}
+import org.cafienne.infrastructure.akka.http.authentication.{AuthenticationDirectives, AuthenticationException, CannotReachIDPException}
 import org.cafienne.service.api
 
 import scala.concurrent.ExecutionContext
@@ -25,32 +26,40 @@ trait AuthenticatedRoute extends CaseServiceRoute {
   val uc = userCache
 
   override def exceptionHandler = ExceptionHandler {
-    case h: UnhealthyCaseSystem => complete(HttpResponse(StatusCodes.ServiceUnavailable, entity = h.getLocalizedMessage))
-    case s: SecurityException =>
-      extractMethod { method =>
-        extractUri { uri =>
-          logger.warn(s"Request ${method.name} $uri has a security issue: " + s)
-          if (logger.underlying.isInfoEnabled()) {
-            logger.info("", s)
-          } else {
-            logger.warn("Enable info logging to see more details on the security issue")
-          }
-          s match {
-            case e: CannotReachIDPException => {
-              logger.error("Service cannot validate security tokens, because IDP is not reachable")
-              complete(HttpResponse(StatusCodes.ServiceUnavailable, entity = e.getMessage)).andThen(g => {
-                // TODO: this probably should be checked upon system startup in the first place
-                //              System.err.println("CANNOT REACH IDP, downing the system")
-                //              System.exit(-1)
-                //              CaseSystem.system.terminate()
-                g
-              })
-            }
-            case _ => complete(HttpResponse(StatusCodes.Unauthorized, entity = s.getMessage()))
-          }
+    case e: CannotReachIDPException => handleIDPException(e)
+    case s: AuthenticationException => handleAuthenticationException(s)
+    case a: AuthorizationException => handleAuthorizationException(a)
+    case i: InvalidCommandException => complete(StatusCodes.BadRequest, i.getMessage)
+    case s: SecurityException => handleAuthorizationException(s) // Pretty weird, as our code does not throw it; log it similar to Authorizaton issues
+    case other => defaultExceptionHandler(other) // All other exceptions just handle the default way from CaseServiceRoute
+  }
+
+  private def handleIDPException(e: CannotReachIDPException) ={
+    logger.error("Service cannot validate security tokens, because IDP is not reachable")
+    complete(HttpResponse(StatusCodes.ServiceUnavailable, entity = e.getMessage)).andThen(g => {
+      // TODO: this probably should be checked upon system startup in the first place
+      //              System.err.println("CANNOT REACH IDP, downing the system")
+      //              System.exit(-1)
+      //              CaseSystem.system.terminate()
+      g
+    })
+  }
+
+  private def handleAuthenticationException(s: AuthenticationException) = {
+    complete(HttpResponse(StatusCodes.Unauthorized, entity = s.getMessage()))
+  }
+
+  private def handleAuthorizationException(s: Exception) = {
+    extractMethod { method =>
+      extractUri { uri =>
+        if (logger.underlying.isInfoEnabled()) {
+          logger.info(s"Authorization issue in request ${method.name} $uri ", s)
+        } else {
+          logger.warn(s"Authorization issue in request ${method.name} $uri (enable info logging for stack trace): " + s.getMessage)
         }
+        complete(HttpResponse(StatusCodes.Unauthorized, entity = s.getMessage()))
       }
-    case other => defaultExceptionHandler(other) // All other exceptions just handle the default way
+    }
   }
 
   // TODO: this is a temporary switch to enable IDE's debugger to show events

case-service/src/main/scala/org/cafienne/infrastructure/akka/http/route/CaseServiceRoute.scala
@@ -67,15 +67,20 @@ trait CaseServiceRoute extends LazyLogging {
   }
 
   def defaultExceptionHandler(t: Throwable): Route = {
-    extractUri { uri =>
-      extractMethod { method =>
-        // Simply print headline of the exception
-        if (logger.underlying.isDebugEnabled()) {
-          logger.debug(s"Bumped into an exception in ${this.getClass().getSimpleName} on ${method.name} $uri:\n" + t, t)
-        } else {
-          logger.info(s"Bumped into an exception in ${this.getClass().getSimpleName} on ${method.name} $uri:\n" + t)
+    t match {
+      case h: UnhealthyCaseSystem => complete(HttpResponse(StatusCodes.ServiceUnavailable, entity = h.getLocalizedMessage))
+      case _ => extractUri { uri =>
+        extractMethod { method =>
+          // Depending on debug logging - either print full exception or only headline
+          if (logger.underlying.isDebugEnabled()) {
+            logger.debug(s"Bumped into an exception in ${this.getClass().getSimpleName} on ${method.name} $uri", t)
+          } else if (logger.underlying.isInfoEnabled()) {
+            logger.info(s"Bumped into an exception in ${this.getClass().getSimpleName} on ${method.name} $uri:\n" + t)
+          } else {
+            logger.warn(s"Bumped into ${t.getClass.getName} in ${this.getClass().getSimpleName} on ${method.name} $uri - enable debug logging for stack trace")
+          }
+          complete(HttpResponse(StatusCodes.InternalServerError))
         }
-        complete(HttpResponse(StatusCodes.InternalServerError))
       }
     }
   }

case-service/src/main/scala/org/cafienne/infrastructure/akka/http/route/UnhealthyCaseSystem.scala
@@ -1,5 +1,3 @@
 package org.cafienne.infrastructure.akka.http.route
 
-class UnhealthyCaseSystem(msg:String) extends Exception(msg) {
-
-}
+class UnhealthyCaseSystem(msg:String) extends Exception(msg)