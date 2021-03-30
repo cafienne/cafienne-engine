4a69c38b1f5c2a53c68446e26e3efbe459add62a
Thijs Petter <thijspetter@gmail.com>
2021-03-30 10:45:16 +0200

    Simple test case on milestone
    This test case shows that milestone transitions are evaluated in the wrong order

---
+++ case-engine/src/test/java/org/cafienne/cmmn/test/sentry/MilestoneTesting.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2014 - 2019 Cafienne B.V.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.cafienne.cmmn.test.sentry;
+
+import org.cafienne.akka.actor.identity.TenantUser;
+import org.cafienne.cmmn.akka.command.StartCase;
+import org.cafienne.cmmn.akka.command.plan.MakeCaseTransition;
+import org.cafienne.cmmn.akka.command.plan.MakePlanItemTransition;
+import org.cafienne.cmmn.akka.command.team.CaseTeam;
+import org.cafienne.cmmn.definition.CaseDefinition;
+import org.cafienne.cmmn.instance.State;
+import org.cafienne.cmmn.instance.Transition;
+import org.cafienne.cmmn.instance.TransitionDeniedException;
+import org.cafienne.cmmn.test.TestScript;
+import org.junit.Test;
+
+public class MilestoneTesting {
+
+    private final String testName = "roles";
+    private final String caseInstanceId = testName;
+    private final TenantUser anonymous = TestScript.getTestUser("user");
+
+    private final CaseDefinition definition = TestScript.getCaseDefinition("testdefinition/milestonetransitions.xml");
+
+    @Test
+    public void testDoubleMilestoneTransition() {
+        TestScript testCase = new TestScript(testName);
+        CaseTeam caseTeam = TestScript.getCaseTeam(TestScript.getOwner(anonymous));
+
+        testCase.addStep(new StartCase(anonymous, caseInstanceId, definition, null, caseTeam), casePlan -> {
+            casePlan.print();
+            casePlan.getEvents().printEventList();
+            casePlan.assertPlanItems("HumanTask").assertSize(1).assertStates(State.Active);
+            System.out.println("It works");
+        });
+
+        testCase.runTest();
+    }
+}
