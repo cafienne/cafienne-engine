47f3f66eaea9ba3d48106864e725af0707e4a830
Thijs Petter <thijspetter@gmail.com>
2021-05-27 17:53:07 +0200

    Remove timer setting no longer in use

--- case-engine/src/main/scala/org/cafienne/akka/actor/config/TimerServiceConfig.scala
+++ case-engine/src/main/scala/org/cafienne/akka/actor/config/TimerServiceConfig.scala
@@ -5,13 +5,6 @@ import org.cafienne.akka.actor.config.util.ChildConfigReader
 class TimerServiceConfig(val parent: EngineConfig) extends ChildConfigReader {
   val path = "timer-service"
 
-  lazy val persistDelay: Long = {
-    val default = 60
-    val period = readLong("persist-delay", default)
-    logger.info("Timer service will persist snapshot changes every " + period + " seconds")
-    period
-  }
-
   /**
     * Returns configuration path for the event store
     */
