536871385cc4232900d9c136953cd238df258416
Thijs Petter <thijspetter@gmail.com>
2021-06-28 13:53:21 +0200

    Invalidate changing an id into multiple new id's

--- case-engine/src/main/scala/org/cafienne/cmmn/actorapi/command/platform/PlatformUpdate.scala
+++ case-engine/src/main/scala/org/cafienne/cmmn/actorapi/command/platform/PlatformUpdate.scala
@@ -1,13 +1,23 @@
 package org.cafienne.cmmn.actorapi.command.platform
 
-import org.cafienne.json.{Value, ValueList}
-import org.cafienne.json.CafienneJson
+import org.cafienne.actormodel.command.exception.InvalidCommandException
+import org.cafienne.json.{CafienneJson, Value, ValueList}
 
 case class PlatformUpdate(info: Seq[NewUserInformation]) extends CafienneJson {
   override def toValue: Value[_] = {
     val list = new ValueList()
     info.map(user => list.add(user.toValue))
     list
+  }
+
+  def validate() = {
+    val existingUserIds = info.map(u => u.existingUserId)
+    // If the list of existing id's is as long as the set it is consisting of unique identifiers.
+    //  If not, then the list is not good, as the same old id might get changed into different new ids.
+    //  We're not checking for duplicates here!
+    if (existingUserIds.size != existingUserIds.toSet.size) {
+      throw new InvalidCommandException("An existing user id cannot be updated into multiple new user ids")
+    }
   }
 
   /** Returns new user info for the specified id if it is present, or else null */
