c742494b646cf64c8675a0606ecbe9c4b6b8d821
Thijs Petter <thijspetter@gmail.com>
2021-12-13 15:07:53 +0100

    Several dependency updates
    spray: 1.3.5 -> 1.3.6
    akka-http: 10.2.4 -> 10.2.6
    akka: 2.6.15 -> 2.6.17
    jackson: 2.12.3 -> 2.13.0
    jasper: 6.17.0 -> 6.18.1
    logback: 1.2.3 -> 1.2.7
    and some more like that

--- project/Deps.scala
+++ project/Deps.scala
@@ -16,14 +16,14 @@ object Deps {
 
   object V {
     val scala       = "2.13.6"
-    val spray       = "1.3.5"
-    val akkaHttp    = "10.2.4"
-    val akka        = "2.6.15"
-    val jackson     = "2.12.3"
+    val spray       = "1.3.6"
+    val akkaHttp    = "10.2.7"
+    val akka        = "2.6.17"
+    val jackson     = "2.13.0"
     val enumeratum  = "1.7.0"
     val swagger     = "2.1.10"
     val slick       = "3.3.3"
-    val jasper      = "6.17.0"
+    val jasper      = "6.18.1"
   }
 
   val akkaActor             = "com.typesafe.akka"       %% "akka-actor"                           % V.akka
@@ -42,11 +42,11 @@ object Deps {
   val akkaMultiNodeTestKit  = "com.typesafe.akka"       %% "akka-multi-node-testkit"              % V.akka
   val akkaSlf4j             = "com.typesafe.akka"       %% "akka-slf4j"                           % V.akka
   val bcrypt                = "com.github.t3hnar"       %% "scala-bcrypt"                         % "4.3.0"
-  val logback               = "ch.qos.logback"          %  "logback-classic"                      % "1.2.3"
+  val logback               = "ch.qos.logback"          %  "logback-classic"                      % "1.2.7"
   val config                = "com.typesafe"            %  "config"                               % "1.4.1"
   val scalaLogging          = "com.typesafe.scala-logging"      %% "scala-logging"                % "3.9.4"
   val enumeratum            = "com.beachape"            %% "enumeratum"                           % V.enumeratum
-  val joseJwt               = "com.nimbusds"            % "nimbus-jose-jwt"                       % "9.10.1"
+  val joseJwt               = "com.nimbusds"            % "nimbus-jose-jwt"                       % "9.15.2"
 
   val akkaHttp              = "com.typesafe.akka"       %% "akka-http"                            % V.akkaHttp
   val akkaHttpXml           = "com.typesafe.akka"       %% "akka-http-xml"                        % V.akkaHttp
@@ -54,11 +54,11 @@ object Deps {
   val akkaHttpCore          = "com.typesafe.akka"       %% "akka-http-core"                       % V.akkaHttp
   val akkaHttpTestkit       = "com.typesafe.akka"       %% "akka-http-testkit"                    % V.akkaHttp
   val akkaHtppJackson       = "com.typesafe.akka"       %% "akka-http-jackson"                    % V.akkaHttp
-  val akkaHttpCors          = "ch.megard"               %% "akka-http-cors"                       % "1.1.1"
+  val akkaHttpCors          = "ch.megard"               %% "akka-http-cors"                       % "1.1.2"
   // As suggested in https://stackoverflow.com/questions/43574426/how-to-resolve-java-lang-noclassdeffounderror-javax-xml-bind-jaxbexception-in-j
   // to resolve blow-up due to swagger :  java.lang.NoClassDefFoundError: javax/xml/bind/annotation/XmlRootElement.
   val javaxws               = "javax.ws.rs"             % "javax.ws.rs-api"                       % "2.1.1"
-  val swaggerAkkaHttp       = "com.github.swagger-akka-http" %% "swagger-akka-http"               % "2.4.2"
+  val swaggerAkkaHttp       = "com.github.swagger-akka-http" %% "swagger-akka-http"               % "2.6.0"
   val swaggerCore           = "io.swagger.core.v3"      % "swagger-core"                          % V.swagger
   val swaggerAnnotations    = "io.swagger.core.v3"      % "swagger-annotations"                   % V.swagger
   val swaggerModels         = "io.swagger.core.v3"      % "swagger-models"                        % V.swagger
@@ -67,7 +67,7 @@ object Deps {
   // The test scope will be added in the build so we don't need to declare it in here
   val scalaMock             = "org.scalamock"           %% "scalamock"                            % "5.1.0"
   val scalaTest             = "org.scalatest"           %% "scalatest"                            % "3.2.9"
-  val junit                 = "org.junit.jupiter"       % "junit-jupiter-api"                     % "5.7.2"
+  val junit                 = "org.junit.jupiter"       % "junit-jupiter-api"                     % "5.8.2"
   val sbtJUnitInterface     = "com.novocode"            % "junit-interface"                       % "0.11"
   val wireMock              = "com.github.tomakehurst"  % "wiremock"                              % "2.27.2"
   val commonsIO             = "commons-io"              %  "commons-io"                           % "20030203.000550"
@@ -76,21 +76,21 @@ object Deps {
   val jacksonDatabind       = "com.fasterxml.jackson.core"   % "jackson-databind"			            % V.jackson
   val jacksonScala          = "com.fasterxml.jackson.module" %% "jackson-module-scala"            % V.jackson
 
-  val spel                  = "org.springframework"     %  "spring-expression"                    % "5.3.8"
+  val spel                  = "org.springframework"     %  "spring-expression"                    % "5.3.13"
   val jsonPath              = "com.jayway.jsonpath"  	  % "json-path"                             % "2.6.0"
 
   val javaMail              = "com.sun.mail"            % "javax.mail"                            % "1.6.2"
-  val ical4j                = "org.mnode.ical4j"        % "ical4j"                                % "3.0.27"
+  val ical4j                = "org.mnode.ical4j"        % "ical4j"                                % "3.1.1"
   val jasperReports         = "net.sf.jasperreports"    % "jasperreports"                         % V.jasper
   val jasperReportFonts     = "net.sf.jasperreports"    % "jasperreports-fonts"                   % V.jasper
   // Lowagie is for PDF document generation with Jasper. It must remain fixed on 2.1.7, because that is what Jasper needs.
   val lowagie               = "com.lowagie"             % "itext"                                 % "2.1.7" // DO NOT CHANGE THIS VALUE
 
-  val sw4jj                 = "com.github.j5ik2o"       %% "sw4jj"                                % "1.1.48"
+  val sw4jj                 = "com.github.j5ik2o"       %% "sw4jj"                                % "1.1.59"
   val slick                 = "com.typesafe.slick"      %% "slick"                                % V.slick
   val hikariCP              = "com.typesafe.slick"      %% "slick-hikaricp"                       % V.slick
-  val postgres              = "org.postgresql"          % "postgresql"                            % "42.2.22"
-  val h2                    = "com.h2database"          % "h2"                                    % "1.4.200"
+  val postgres              = "org.postgresql"          % "postgresql"                            % "42.3.1"
+  val h2                    = "com.h2database"          % "h2"                                    % "2.0.202"
   val hsqldb                = "org.hsqldb"              % "hsqldb"                                % "2.5.1"
   val sqlserver             = "com.microsoft.sqlserver" % "mssql-jdbc"                            % "9.2.1.jre11"
   val flyway                = "org.flywaydb"            % "flyway-core"                           % "7.2.1"
@@ -105,5 +105,4 @@ object Deps {
     val recorder            = "io.gatling"              %  "gatling-recorder"                     % v
     val charts              = "io.gatling.highcharts"   %  "gatling-charts-highcharts"            % v
   }
-
 }
