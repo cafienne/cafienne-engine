4b498c9b2a69671f704cb3c469f9729e2db04c7d
Thijs Petter <thijspetter@gmail.com>
2022-01-14 16:30:39 +0100

    Restrict CafienneGateway to ModelCommand messages only

--- case-engine/src/main/scala/org/cafienne/system/router/CafienneGateway.scala
+++ case-engine/src/main/scala/org/cafienne/system/router/CafienneGateway.scala
@@ -2,6 +2,7 @@ package org.cafienne.system.router
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.util.Timeout
+import org.cafienne.actormodel.command.ModelCommand
 import org.cafienne.infrastructure.Cafienne
 import org.cafienne.system.CaseSystem
 
@@ -11,14 +12,29 @@ class CafienneGateway(caseSystem: CaseSystem) {
   private val system: ActorSystem = caseSystem.system
   val messageRouterService: ActorRef = system.actorOf(Props.create(classOf[LocalRouter], caseSystem), "LocalRouter")
 
-  def request(message: Any): Future[Any] = {
+  val caseRouterService = system.actorOf(Props.create(classOf[LocalRouter], caseSystem), "CaseRouter")
+  val processRouterService = system.actorOf(Props.create(classOf[LocalRouter], caseSystem), "ProcessTaskRouter")
+  val tenantRouterService = system.actorOf(Props.create(classOf[LocalRouter], caseSystem), "TenantRouter")
+  val consentGroupRouterService = system.actorOf(Props.create(classOf[LocalRouter], caseSystem), "ConsentGroupRouter")
+
+  private def getRouter(message: Any): ActorRef = {
+    message match {
+      case _: CaseCommand =>  caseRouterService
+      case _: ProcessCommand => processRouterService
+      case _: TenantCommand => tenantRouterService
+      case _: ConsentGroupCommand => consentGroupRouterService
+      case _ => messageRouterService
+    }
+  }
+
+  def request(message: ModelCommand): Future[Any] = {
     import akka.pattern.ask
     implicit val timeout: Timeout = Cafienne.config.actor.askTimout
 
     messageRouterService.ask(message)
   }
 
-  def inform(msg: Any, sender: ActorRef = Actor.noSender): Unit = {
-    messageRouterService.tell(msg, sender)
+  def inform(message: ModelCommand, sender: ActorRef = Actor.noSender): Unit = {
+    getRouter(message).tell(message, sender)
   }
 }
