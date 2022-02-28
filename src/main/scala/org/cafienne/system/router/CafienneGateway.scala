8c02c088e7e015d7b83eca20962431ba3f9c0a97
Thijs Petter <thijspetter@gmail.com>
2022-02-28 13:04:19 +0100

    Remove unused code from CaseMessageRouter
    Also applied formatter to Gateway

--- src/main/scala/org/cafienne/system/router/CafienneGateway.scala
+++ src/main/scala/org/cafienne/system/router/CafienneGateway.scala
@@ -21,16 +21,6 @@ class CafienneGateway(caseSystem: CaseSystem) {
   private val consentGroupService = system.actorOf(Props.create(classOf[LocalRouter], caseSystem, actors), "consent-groups")
   private val defaultRouterService: ActorRef = system.actorOf(Props.create(classOf[LocalRouter], caseSystem, actors), "default-router")
 
-  private def getRouter(message: Any): ActorRef = {
-    message match {
-      case _: CaseCommand =>  caseService
-      case _: ProcessCommand => processTaskService
-      case _: TenantCommand => tenantService
-      case _: ConsentGroupCommand => consentGroupService
-      case _ => defaultRouterService
-    }
-  }
-
   def request(message: ModelCommand): Future[Any] = {
     import akka.pattern.ask
     implicit val timeout: Timeout = Cafienne.config.actor.askTimout
@@ -40,5 +30,15 @@ class CafienneGateway(caseSystem: CaseSystem) {
 
   def inform(message: ModelCommand, sender: ActorRef = Actor.noSender): Unit = {
     getRouter(message).tell(message, sender)
+  }
+
+  private def getRouter(message: Any): ActorRef = {
+    message match {
+      case _: CaseCommand => caseService
+      case _: ProcessCommand => processTaskService
+      case _: TenantCommand => tenantService
+      case _: ConsentGroupCommand => consentGroupService
+      case _ => defaultRouterService
+    }
   }
 }
