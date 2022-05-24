9ed2070cbefdfa280e8fc69be5c8d24b91b498e2
Thijs Petter <thijspetter@gmail.com>
2022-05-24 13:43:23 +0200

    Restructure EventBatchSource for parallellism
    In JDBC based plugins, Source of eventsByTag can stream multiple events from different persistenceId's. Now keeping a map of EventBatch per persistence id, instead of only one batch as an Option.
    Note: this may lead to pumping of memory, but before the EventBatching commit something similar was possible, as that also kept a Map of CaseTransaction (and TenantTransaction, etc) per eventsByTag() Source.
    
    Fixes #327

--- src/main/scala/org/cafienne/infrastructure/cqrs/batch/EventBatchSource.scala
+++ src/main/scala/org/cafienne/infrastructure/cqrs/batch/EventBatchSource.scala
@@ -26,42 +26,38 @@ trait EventBatchSource extends TaggedEventSource with LazyLogging {
 
   def batches: Source[EventBatch, NotUsed] = {
     taggedEvents
-      .via(new Magic.BatchingFlow) // Join all ModelEvents that resulted from a single ModelCommand into a batch
+      .via(new BatchingFlow) // Join all ModelEvents that resulted from a single ModelCommand into a batch
       .filter(_.isDefined).map(_.get) // Is it possible to do this inside the batching flow?
   }
 
-  object Magic {
-    class BatchingFlow(converter: => ModelEventEnvelope => Option[EventBatch] = (new EventBatcher())) extends GraphStage[FlowShape[ModelEventEnvelope, Option[EventBatch]]] {
-      val in: Inlet[ModelEventEnvelope] = Inlet[ModelEventEnvelope]("ModelEvent.BatchingFlow.in")
-      val out: Outlet[Option[EventBatch]] = Outlet[Option[EventBatch]]("ModelEvent.BatchingFlow.out")
-      val shape: FlowShape[ModelEventEnvelope, Option[EventBatch]] = FlowShape.of(in, out)
+  class BatchingFlow extends GraphStage[FlowShape[ModelEventEnvelope, Option[EventBatch]]] {
+    val in: Inlet[ModelEventEnvelope] = Inlet[ModelEventEnvelope]("ModelEvent.BatchingFlow.in")
+    val out: Outlet[Option[EventBatch]] = Outlet[Option[EventBatch]]("ModelEvent.BatchingFlow.out")
+    val shape: FlowShape[ModelEventEnvelope, Option[EventBatch]] = FlowShape.of(in, out)
 
-      override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
-        val f: ModelEventEnvelope => Option[EventBatch] = converter
-        setHandler(in, () => push(out, f(grab(in))))
-        setHandler(out, () => pull(in))
-      }
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+      val f: ModelEventEnvelope => Option[EventBatch] = updateBatch
+      setHandler(in, () => push(out, f(grab(in))))
+      setHandler(out, () => pull(in))
     }
 
-    class EventBatcher extends (ModelEventEnvelope => Option[EventBatch]) with LazyLogging {
-      private var currentBatch: Option[EventBatch] = None
-      override def apply(envelope: ModelEventEnvelope): Option[EventBatch] = {
-        if (currentBatch.isEmpty || envelope.persistenceId != currentBatch.get.persistenceId) {
-          // We're about to get some new events
-          currentBatch = Some(createBatch(envelope.persistenceId))
-        }
-        currentBatch.get.addEvent(envelope)
-        if (envelope.event.isInstanceOf[CommitEvent]) {
-          val response = currentBatch
-          logger.whenDebugEnabled({
-            val batch = currentBatch.get
-            logger.debug(s"Received batch with ${batch.events.size} events of types ${batch.events.map(_.event.getClass.getSimpleName).toSet.mkString(", ")}")
-          })
-          currentBatch = None
-          response
-        } else {
-          None
-        }
+    import scala.collection.mutable
+
+    // Map with batches per persistenceId that are currently streaming events to us.
+    private val currentBatches = new mutable.HashMap[String, EventBatch]()
+
+    private def updateBatch(envelope: ModelEventEnvelope): Option[EventBatch] = {
+      val persistenceId = envelope.persistenceId
+      // Retrieve existing batch, or create a new one
+      val batch = currentBatches.getOrElseUpdate(persistenceId, createBatch(persistenceId))
+      // Add the event, and if it is a CommitEvent, then the batch is complete and can be sent further
+      batch.addEvent(envelope)
+      if (envelope.event.isInstanceOf[CommitEvent]) {
+        logger.whenDebugEnabled(logger.debug(s"Received batch with ${batch.events.size} events of types ${batch.events.map(_.event.getClass.getSimpleName).toSet.mkString(", ")}"))
+        currentBatches.remove(persistenceId)
+        Some(batch)
+      } else {
+        None
       }
     }
   }
