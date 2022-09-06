cc36265f43e111910b63ffc16ff2573d00bd576c
Thijs Petter <thijspetter@gmail.com>
2022-09-06 16:08:37 +0200

    Make TimerEvent extend PlanItemEvent

--- src/main/java/org/cafienne/cmmn/actorapi/event/plan/eventlistener/TimerBaseEvent.java
+++ src/main/java/org/cafienne/cmmn/actorapi/event/plan/eventlistener/TimerBaseEvent.java
@@ -2,9 +2,10 @@ package org.cafienne.cmmn.actorapi.event.plan.eventlistener;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import org.cafienne.actormodel.event.ModelEvent;
-import org.cafienne.cmmn.actorapi.event.CaseBaseEvent;
 import org.cafienne.cmmn.actorapi.event.CaseEvent;
+import org.cafienne.cmmn.actorapi.event.plan.PlanItemEvent;
 import org.cafienne.cmmn.instance.Case;
+import org.cafienne.cmmn.instance.PlanItem;
 import org.cafienne.cmmn.instance.TimerEvent;
 import org.cafienne.infrastructure.serialization.Fields;
 import org.cafienne.json.ValueMap;
@@ -16,10 +17,10 @@ import java.util.Set;
  * Base class for all TimerEventListener based akka events.
  * These events are tagged so that a projection can handle them and safely set timers.
  */
-public abstract class TimerBaseEvent extends CaseBaseEvent {
+public abstract class TimerBaseEvent extends PlanItemEvent {
     public static final String TAG = "cafienne:timer";
 
-    private static Set<String> tags = Set.of(ModelEvent.TAG, CaseEvent.TAG, TimerBaseEvent.TAG);
+    private static final Set<String> tags = Set.of(ModelEvent.TAG, CaseEvent.TAG, TimerBaseEvent.TAG);
 
     @Override
     public Set<String> tags() {
@@ -29,7 +30,7 @@ public abstract class TimerBaseEvent extends CaseBaseEvent {
     private final String timerId;
 
     public TimerBaseEvent(TimerEvent timerEvent) {
-        super(timerEvent.getCaseInstance());
+        super(timerEvent);
         this.timerId = timerEvent.getId();
     }
 
@@ -44,16 +45,22 @@ public abstract class TimerBaseEvent extends CaseBaseEvent {
 
     @Override
     public void updateState(Case actor) {
+        // Nothing to update. Avoid parent class update that searches for unnecessary plan item
+    }
+
+    @Override
+    protected void updatePlanItemState(PlanItem<?> planItem) {
+        // Nothing to update.
     }
 
     @Override
     public void write(JsonGenerator generator) throws IOException {
-        super.writeCaseEvent(generator);
+        super.writePlanItemEvent(generator);
         writeField(generator, Fields.timerId, timerId);
     }
 
     @Override
     public String toString() {
-        return "Timer "+getTimerId()+" has completed";
+        return "Timer " + getTimerId() + " has completed";
     }
 }
