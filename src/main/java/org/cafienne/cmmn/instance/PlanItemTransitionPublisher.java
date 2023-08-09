eecda01a7ffde883f24a842ebd59b8296e97d30c
Thijs Petter <thijspetter@gmail.com>
2023-08-09 12:10:08 +0200

    Make classes public because they are exposed
    Fixes an IntelliJ warning

--- src/main/java/org/cafienne/cmmn/instance/PlanItemTransitionPublisher.java
+++ src/main/java/org/cafienne/cmmn/instance/PlanItemTransitionPublisher.java
@@ -21,7 +21,7 @@ import org.cafienne.cmmn.actorapi.event.plan.PlanItemTransitioned;
 import org.cafienne.cmmn.instance.sentry.PlanItemOnPart;
 import org.cafienne.cmmn.instance.sentry.TransitionPublisher;
 
-class PlanItemTransitionPublisher extends TransitionPublisher<PlanItemTransitioned, PlanItem<?>, PlanItemOnPart> {
+public class PlanItemTransitionPublisher extends TransitionPublisher<PlanItemTransitioned, PlanItem<?>, PlanItemOnPart> {
     PlanItemTransitionPublisher(PlanItem<?> item) {
         super(item);
     }
--- src/main/java/org/cafienne/cmmn/instance/casefile/CaseFileTransitionPublisher.java
+++ src/main/java/org/cafienne/cmmn/instance/casefile/CaseFileTransitionPublisher.java
@@ -21,7 +21,7 @@ import org.cafienne.cmmn.actorapi.event.file.CaseFileItemTransitioned;
 import org.cafienne.cmmn.instance.sentry.CaseFileItemOnPart;
 import org.cafienne.cmmn.instance.sentry.TransitionPublisher;
 
-class CaseFileTransitionPublisher extends TransitionPublisher<CaseFileItemTransitioned, CaseFileItem, CaseFileItemOnPart> {
+public class CaseFileTransitionPublisher extends TransitionPublisher<CaseFileItemTransitioned, CaseFileItem, CaseFileItemOnPart> {
     CaseFileTransitionPublisher(CaseFileItem item) {
         super(item);
     }
