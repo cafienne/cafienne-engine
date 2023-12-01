414a2b0981bddb56724780afe766801f479615d2
Thijs Petter <thijspetter@gmail.com>
2023-12-01 08:08:41 +0100

    Revert "Extend CaseMembership with owner information"
    This reverts commit f0215cc8711b6de7fb83e559d44ac9095972b78b.
    
    It leads to Slick exceptions on NULL fields

--- src/main/scala/org/cafienne/querydb/query/CaseMembership.scala
+++ src/main/scala/org/cafienne/querydb/query/CaseMembership.scala
@@ -19,4 +19,4 @@ package org.cafienne.querydb.query
 
 import org.cafienne.actormodel.identity.{CaseUserIdentity, ConsentGroupMembership, Origin}
 
-class CaseMembership(override val id: String, val isOwner: Boolean = true, override val origin: Origin, override val tenantRoles: Set[String], override val groups: Seq[ConsentGroupMembership], val caseInstanceId: String, val tenant: String) extends CaseUserIdentity
+class CaseMembership(override val id: String, override val origin: Origin, override val tenantRoles: Set[String], override val groups: Seq[ConsentGroupMembership], val caseInstanceId: String, val tenant: String) extends CaseUserIdentity
