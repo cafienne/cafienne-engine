9b6f4b307b9be56b7ca49c63875a04ea4c5b0667
Thijs Petter <thijspetter@gmail.com>
2024-02-23 17:23:28 +0100

    Move comparison logic into DefinitionElement

--- src/main/java/org/cafienne/cmmn/definition/DefinitionElement.java
+++ src/main/java/org/cafienne/cmmn/definition/DefinitionElement.java
@@ -20,6 +20,7 @@ package org.cafienne.cmmn.definition;
 import org.cafienne.processtask.definition.ProcessDefinition;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Basic element for definitions
@@ -85,6 +86,30 @@ public interface DefinitionElement {
     boolean differs(DefinitionElement object);
 
     /**
+     * Custom compare method. Comparable to Object.equals(), but elements are expected
+     * to implement a semantic comparison.
+     */
+    boolean equalsWith(Object object);
+
+    default boolean sameName(XMLElementDefinition other) {
+        return same(getName(), other.getName());
+    }
+
+    default boolean sameId(XMLElementDefinition other) {
+        return same(this.getId(), other.getId());
+    }
+
+    /**
+     * Check whether name and id match on the other element.
+     */
+    default boolean sameIdentifiers(XMLElementDefinition other) {
+        return sameName(other) && sameId(other);
+    }
+
+    default boolean similarIdentifiers(XMLElementDefinition other) {
+        return sameId(other) || sameName(other);
+    }
+    /**
      * Returns true if this element has the identifier in its name or id attribute.
      */
     default boolean hasIdentifier(String identifier) {
@@ -114,5 +139,84 @@ public interface DefinitionElement {
             }
         }
         return null;
+    }
+
+    default boolean sameClass(Object object) {
+        return object != null && this.getClass().equals(object.getClass());
+    }
+
+    default boolean same(String obj1, String obj2) {
+        return Objects.equals(obj1, obj2);
+    }
+
+    default <E extends Enum<?>> boolean same(E obj1, E obj2) {
+        return Objects.equals(obj1, obj2);
+    }
+
+    default boolean same(boolean obj1, boolean obj2) {
+        return Objects.equals(obj1, obj2);
+    }
+
+    /**
+     * Shortcut to Objects.equals()
+     */
+    default boolean same(Object obj1, Object obj2) {
+        return Objects.equals(obj1, obj2);
+    }
+
+    /**
+     * This checks that other has Class[E] and invokes the matcher on it
+     */
+    default <E extends DefinitionElement> boolean equalsWith(Object object, DefinitionElementComparer<E> matcher) {
+//        System.out.println("Running EW WIHT MATCHER ON A " + this.getClass().getSimpleName() +" with name " + getName());
+        if (!sameClass(object)) {
+            return false;
+        }
+
+        // TODO: it will be good if we can create a comparison report (a tree) on the definition and log that.
+        String currentIndent = Indenter.indent;
+        Indenter.indent += "  ";
+        boolean isEqual = matcher.match((E) object);
+        Indenter.indent = currentIndent;
+//        System.out.println(indent + "Comparison result for " + this.getClass().getSimpleName() +"[" + getName() +"] is " + isEqual);
+        return isEqual;
+    }
+
+    /**
+     * Check equality of both collections
+     *
+     * @param ours   Source to compare
+     * @param theirs Target to compare against
+     * @param <X>    Type to compare about
+     * @return false if sizes differ or at leas one element differs.
+     */
+    default <X extends DefinitionElement> boolean same(Collection<X> ours, Collection<X> theirs) {
+        if (ours.size() != theirs.size()) {
+            return false;
+        }
+        for (X mine : ours) { // Iterate all our elements, and check if there is one that does not match any of theirs. If so, return false.
+            if (DefinitionElement.findDefinition(mine, theirs) == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Determine whether both definition elements are the same.
+     * Done through regular null check and then invoking the equalsWith method.
+     */
+    default <X extends DefinitionElement> boolean same(X obj1, X obj2) {
+//        if (obj1 != null) System.out.println("Calling s on " + obj1.getClass().getSimpleName());
+        return obj1 == null && obj2 == null || obj1 != null && obj2 != null && obj1.equalsWith(obj2);
+    }
+
+    @FunctionalInterface
+    interface DefinitionElementComparer<X extends DefinitionElement> {
+        boolean match(X other);
+    }
+
+    class Indenter {
+        static String indent = "";
     }
 }
