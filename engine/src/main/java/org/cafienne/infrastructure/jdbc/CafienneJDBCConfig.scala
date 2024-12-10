8bdceac6b67af0f7dc52c03e609336787172b23e
Thijs Petter <thijspetter@gmail.com>
2024-12-10 18:50:51 +0100

    Generate shorter index names for JDBC
    If we're using table prefixes, then in e.g. PostgreSQL the index names may become longer than 64 characters, causing duplicate index names (as the db cuts off the name after 64 characters).

--- engine/src/main/java/org/cafienne/infrastructure/jdbc/CafienneJDBCConfig.scala
+++ engine/src/main/java/org/cafienne/infrastructure/jdbc/CafienneJDBCConfig.scala
@@ -30,17 +30,18 @@ import slick.sql.SqlAction
 import slick.sql.SqlProfile.ColumnOption.SqlType
 
 /**
-  * Basic JDBC abstraction on Slick that can be used to hook a database connection
-  * based on config properties.
-  *
-  * Includes some helpers for queries and MS SQL Server support
-  */
+ * Basic JDBC abstraction on Slick that can be used to hook a database connection
+ * based on config properties.
+ *
+ * Includes some helpers for queries and MS SQL Server support
+ */
 trait CafienneJDBCConfig {
   lazy val dbConfig: DatabaseConfig[JdbcProfile] = ???
 
   lazy val db = dbConfig.db
 
   import dbConfig.profile.api._
+
   lazy val isSQLServer = dbConfig.profile.isInstanceOf[slick.jdbc.SQLServerProfile]
 
   val prefix: String = Cafienne.config.persistence.tablePrefix
@@ -48,22 +49,24 @@ trait CafienneJDBCConfig {
   abstract class CafienneTable[T](tag: Tag, name: String) extends Table[T](tag, prefix + name) {
 
     /**
-      * If queries on the table use the Sort case class, then the table must implement this method
-      * @param field
-      * @return
-      */
+     * If queries on the table use the Sort case class, then the table must implement this method
+     *
+     * @param field
+     * @return
+     */
     def getSortColumn(field: String): ColumnOrdered[_] = ???
 
     /**
-      * Creates a String column with the specified name and options.
-      * Overrides O.Length and SqlType options, and replaces with NVARCHAR(255).
-      * This method can be used in primary key fields and foreign key fields of type String
-      * Typically in fields that hold actor id's, such as Tenant, CaseInstanceId, or TaskId
-      * @param columnName
-      * @param options
-      * @tparam T
-      * @return
-      */
+     * Creates a String column with the specified name and options.
+     * Overrides O.Length and SqlType options, and replaces with NVARCHAR(255).
+     * This method can be used in primary key fields and foreign key fields of type String
+     * Typically in fields that hold actor id's, such as Tenant, CaseInstanceId, or TaskId
+     *
+     * @param columnName
+     * @param options
+     * @tparam T
+     * @return
+     */
     def idColumn[C](columnName: String, options: ColumnOption[String]*) = {
       // NOTE: This code is invoked many times, also continuously during runtime
 
@@ -80,24 +83,32 @@ trait CafienneJDBCConfig {
           }
         }
       }
-      super.column[String](columnName, newOptions:_*)
+      super.column[String](columnName, newOptions: _*)
     }
 
     /**
-      * Creates an index on the table on the column, with a standard generation of the index name.
-      * @param column
-      * @return
-      */
+     * Creates an index on the table on the column, with a standard generation of the index name.
+     *
+     * @param column
+     * @return
+     */
     def oldStyleIndex(column: Rep[String]): Index = {
       index(oldStyleIxName(column), column)
     }
 
     /**
-      * Generate an index name for the specified column
-      *
-      */
+     * Generate an index name for the specified column
+     *
+     */
     def oldStyleIxName(column: Rep[_]): String = {
-      s"ix_${tableName}__$column"
+      if (prefix.isEmpty) {
+        // Old style index generation includes table-name twice. When we also add the prefix it may become too long.
+        //  When there is a prefix we make the index name shorter.
+        s"ix_${tableName}__$column"
+      } else {
+        // Use the new ixName method that gives shorted index names
+        ixName(column)
+      }
     }
 
     def index(column: Rep[String]): Index = {
@@ -105,8 +116,8 @@ trait CafienneJDBCConfig {
     }
 
     /**
-      * Generate an index name for the specified column
-      */
+     * Generate an index name for the specified column
+     */
     def ixName(column: Rep[_]): String = {
       val columnName = s"$column".replace(s"$tableName.", "")
       val name = s"ix_${tableName}__$columnName"
@@ -115,20 +126,22 @@ trait CafienneJDBCConfig {
     }
 
     /**
-      * Generate a primary key name based on the table name (to make it unique across the database)
-      * @return
-      */
+     * Generate a primary key name based on the table name (to make it unique across the database)
+     *
+     * @return
+     */
     def pkName: String = {
       s"pk_$tableName"
     }
 
     /**
-      * Similar to idColumn, but now for storing userId's. Defaults to 255 as length.
-      * @param columnName
-      * @param options
-      * @tparam T
-      * @return
-      */
+     * Similar to idColumn, but now for storing userId's. Defaults to 255 as length.
+     *
+     * @param columnName
+     * @param options
+     * @tparam T
+     * @return
+     */
     def userColumn[C](columnName: String, options: ColumnOption[String]*) = {
       // NOTE: This code is invoked many times, also continuously during runtime
 
@@ -149,13 +162,14 @@ trait CafienneJDBCConfig {
     }
 
     /**
-      * Similar to idColumn, but now for storing states and transitions (technical data). Defaults to 16 as length.
-      * (ParentTerminate is longest value as of now)
-      * @param columnName
-      * @param options
-      * @tparam T
-      * @return
-      */
+     * Similar to idColumn, but now for storing states and transitions (technical data). Defaults to 16 as length.
+     * (ParentTerminate is longest value as of now)
+     *
+     * @param columnName
+     * @param options
+     * @tparam T
+     * @return
+     */
     def stateColumn[C](columnName: String, options: ColumnOption[String]*) = {
       // NOTE: This code is invoked many times, also continuously during runtime
 
@@ -177,12 +191,13 @@ trait CafienneJDBCConfig {
 
 
     /**
-      * Column that can hold json information; NVARCHAR(MAX) by default
-      * @param columnName
-      * @param options
-      * @tparam T
-      * @return
-      */
+     * Column that can hold json information; NVARCHAR(MAX) by default
+     *
+     * @param columnName
+     * @param options
+     * @tparam T
+     * @return
+     */
     def jsonColumn[C](columnName: String, options: ColumnOption[String]*) = {
       // NOTE: This code is invoked many times, also continuously during runtime
 
@@ -204,23 +219,25 @@ trait CafienneJDBCConfig {
   }
 
   /**
-    * Base class for tables that have a 'tenant' column
-    * @param tag
-    * @param tableName
-    * @tparam T
-    */
+   * Base class for tables that have a 'tenant' column
+   *
+   * @param tag
+   * @param tableName
+   * @tparam T
+   */
   abstract class CafienneTenantTable[T](tag: Tag, tableName: String) extends CafienneTable[T](tag, tableName) {
     def tenant = idColumn[String]("tenant")
   }
 
   implicit class QueryHelper[CT <: CafienneTable[_], E](query: Query[CT, E, Seq]) {
     /**
-      * Orders the results as given in the Sort object.
-      * Note that if the Sort.on field is empty (None), the query will not be affected.
-      * Default sort order is descending
-      * @param sort
-      * @return
-      */
+     * Orders the results as given in the Sort object.
+     * Note that if the Sort.on field is empty (None), the query will not be affected.
+     * Default sort order is descending
+     *
+     * @param sort
+     * @return
+     */
     def order(sort: Sort): Query[CT, E, Seq] = {
       sort.on.fold(query)(fieldName => sort.ascending match {
         case true => query.sortBy(_.getSortColumn(fieldName.toLowerCase).asc)
@@ -229,10 +246,11 @@ trait CafienneJDBCConfig {
     }
 
     /**
-      * Only select records from a certain offset and up to a certain number of results
-      * @param area
-      * @return
-      */
+     * Only select records from a certain offset and up to a certain number of results
+     *
+     * @param area
+     * @return
+     */
     def only(area: Area): Query[CT, E, Seq] = {
       query.drop(area.offset).take(area.numOfResults)
     }
@@ -240,12 +258,12 @@ trait CafienneJDBCConfig {
 
   implicit class TenantQueryHelper[CTT <: CafienneTenantTable[_], E](query: Query[CTT, E, Seq]) {
     /**
-      * Add tenant selector to the query. If no tenants specified, it will not add a filter on tenant
-      * and search across tenants
-      *
-      * @param tenants
-      * @return
-      */
+     * Add tenant selector to the query. If no tenants specified, it will not add a filter on tenant
+     * and search across tenants
+     *
+     * @param tenants
+     * @return
+     */
     def inTenants(tenants: Option[Seq[String]]): Query[CTT, E, Seq] = {
       val set = tenants.getOrElse(Seq())
       set.isEmpty match {
@@ -263,22 +281,24 @@ trait CafienneJDBCConfig {
   }
 
   /**
-    * Helper to create SqlMigration object from a 'regular' Slick SqlAction
-    * @param action
-    * @return
-    */
-  def asSqlMigration(action: SqlAction[_,_,_]): MigrationSeq = {
-    asSqlMigration(action.statements.toSeq:_*)
+   * Helper to create SqlMigration object from a 'regular' Slick SqlAction
+   *
+   * @param action
+   * @return
+   */
+  def asSqlMigration(action: SqlAction[_, _, _]): MigrationSeq = {
+    asSqlMigration(action.statements.toSeq: _*)
   }
 
   /**
-    * Helper to create SqlMigration from one or more strings
-    * @param action
-    * @return
-    */
+   * Helper to create SqlMigration from one or more strings
+   *
+   * @param action
+   * @return
+   */
   def asSqlMigration(sql: String*): MigrationSeq = {
-//    val statements = sql.mkString(";\n")
-//    println(s"SQL:\n\t$statements\n")
-    MigrationSeq(sql.map(statement => SqlMigration(statement)):_*)
+    //    val statements = sql.mkString(";\n")
+    //    println(s"SQL:\n\t$statements\n")
+    MigrationSeq(sql.map(statement => SqlMigration(statement)): _*)
   }
 }
