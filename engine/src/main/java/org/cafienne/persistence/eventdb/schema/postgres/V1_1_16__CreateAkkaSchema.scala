e050dc9f14e05983bb3491348a3076fc6abde9fb
Thijs Petter <thijspetter@gmail.com>
2025-02-11 14:02:15 +0100

    Introduce non-static EventDB.tablePrefix property
    Instead of taking the prefix from Cafienne.config
    
    Also convert it to a class

--- engine/src/main/java/org/cafienne/persistence/eventdb/schema/postgres/V1_1_16__CreateAkkaSchema.scala
+++ engine/src/main/java/org/cafienne/persistence/eventdb/schema/postgres/V1_1_16__CreateAkkaSchema.scala
@@ -1,9 +1,8 @@
 package org.cafienne.persistence.eventdb.schema.postgres
 
-import org.cafienne.infrastructure.Cafienne
 import org.cafienne.persistence.eventdb.schema.ClassicEventDBSchemaScript
 
-class V1_1_16__CreateAkkaSchema extends ClassicEventDBSchemaScript {
+class V1_1_16__CreateAkkaSchema(tablePrefix: String) extends ClassicEventDBSchemaScript {
   val version = "1.1.16"
 
   val description = "CreateAkkaSchema"
@@ -13,7 +12,7 @@ class V1_1_16__CreateAkkaSchema extends ClassicEventDBSchemaScript {
   override def getChecksum: Integer = 1702145590
 
   override def sql: String = {
-    s"""CREATE TABLE IF NOT EXISTS public.${Cafienne.config.persistence.tablePrefix}event_journal(
+    s"""CREATE TABLE IF NOT EXISTS public.${tablePrefix}event_journal(
        |  ordering BIGSERIAL,
        |  persistence_id VARCHAR(255) NOT NULL,
        |  sequence_number BIGINT NOT NULL,
@@ -34,19 +33,19 @@ class V1_1_16__CreateAkkaSchema extends ClassicEventDBSchemaScript {
        |  PRIMARY KEY(persistence_id, sequence_number)
        |);
        |
-       |CREATE UNIQUE INDEX ${Cafienne.config.persistence.tablePrefix}event_journal_ordering_idx ON public.${Cafienne.config.persistence.tablePrefix}event_journal(ordering);
+       |CREATE UNIQUE INDEX ${tablePrefix}event_journal_ordering_idx ON public.${tablePrefix}event_journal(ordering);
        |
-       |CREATE TABLE IF NOT EXISTS public.${Cafienne.config.persistence.tablePrefix}event_tag(
+       |CREATE TABLE IF NOT EXISTS public.${tablePrefix}event_tag(
        |    event_id BIGINT,
        |    tag VARCHAR(256),
        |    PRIMARY KEY(event_id, tag),
-       |    CONSTRAINT fk_${Cafienne.config.persistence.tablePrefix}event_journal
+       |    CONSTRAINT fk_${tablePrefix}event_journal
        |      FOREIGN KEY(event_id)
-       |      REFERENCES ${Cafienne.config.persistence.tablePrefix}event_journal(ordering)
+       |      REFERENCES ${tablePrefix}event_journal(ordering)
        |      ON DELETE CASCADE
        |);
        |
-       |CREATE TABLE IF NOT EXISTS public.${Cafienne.config.persistence.tablePrefix}snapshot (
+       |CREATE TABLE IF NOT EXISTS public.${tablePrefix}snapshot (
        |  persistence_id VARCHAR(255) NOT NULL,
        |  sequence_number BIGINT NOT NULL,
        |  created BIGINT NOT NULL,
@@ -62,7 +61,7 @@ class V1_1_16__CreateAkkaSchema extends ClassicEventDBSchemaScript {
        |  PRIMARY KEY(persistence_id, sequence_number)
        |);
        |
-       |CREATE TABLE IF NOT EXISTS public.${Cafienne.config.persistence.tablePrefix}durable_state (
+       |CREATE TABLE IF NOT EXISTS public.${tablePrefix}durable_state (
        |    global_offset BIGSERIAL,
        |    persistence_id VARCHAR(255) NOT NULL,
        |    revision BIGINT NOT NULL,
@@ -73,8 +72,8 @@ class V1_1_16__CreateAkkaSchema extends ClassicEventDBSchemaScript {
        |    state_timestamp BIGINT NOT NULL,
        |    PRIMARY KEY(persistence_id)
        |    );
-       |CREATE INDEX ${Cafienne.config.persistence.tablePrefix}state_tag_idx on public.${Cafienne.config.persistence.tablePrefix}durable_state (tag);
-       |CREATE INDEX ${Cafienne.config.persistence.tablePrefix}state_global_offset_idx on public.${Cafienne.config.persistence.tablePrefix}durable_state (global_offset);
+       |CREATE INDEX ${tablePrefix}state_tag_idx on public.${tablePrefix}durable_state (tag);
+       |CREATE INDEX ${tablePrefix}state_global_offset_idx on public.${tablePrefix}durable_state (global_offset);
        |""".stripMargin
   }
 }
