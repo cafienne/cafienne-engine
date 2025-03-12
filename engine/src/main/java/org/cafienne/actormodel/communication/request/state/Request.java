9f2e9d060ca56baa4f5b73f9280852cd34aef739
Thijs Petter <thijspetter@gmail.com>
2025-03-12 13:29:10 +0100

    Add retry mechanism with timer

--- engine/src/main/java/org/cafienne/actormodel/communication/request/state/Request.java
+++ engine/src/main/java/org/cafienne/actormodel/communication/request/state/Request.java
@@ -1,17 +1,22 @@
 package org.cafienne.actormodel.communication.request.state;
 
+import org.cafienne.actormodel.command.ModelCommand;
 import org.cafienne.actormodel.communication.request.command.RequestModelActor;
 import org.cafienne.actormodel.communication.request.event.ActorRequestCreated;
 import org.cafienne.actormodel.communication.request.event.ActorRequestDelivered;
 import org.cafienne.actormodel.communication.request.event.ModelActorReplyEvent;
+import org.cafienne.processtask.actorapi.command.MigrateProcessDefinition;
+import org.cafienne.processtask.actorapi.command.ReactivateProcess;
 
 class Request {
     private final RemoteActorState<?> state;
+    private final RequestTracker tracker;
     private ActorRequestCreated creationEvent;
     private ActorRequestDelivered deliveryEvent;
 
     Request(RemoteActorState<?> state) {
         this.state = state;
+        this.tracker = new RequestTracker(this, state.actor.caseSystem);
     }
 
     void updateState(ModelActorReplyEvent event) {
@@ -20,6 +25,10 @@ class Request {
             case ActorRequestDelivered ard -> delivered(ard);
             default -> System.out.println("BOOM on a " + event.getClass().getName());
         }
+    }
+
+    public ModelCommand getCommand() {
+        return creationEvent.command;
     }
 
     public void created(ActorRequestCreated event) {
@@ -32,11 +41,13 @@ class Request {
     public void delivered(ActorRequestDelivered event) {
 //        System.out.println(this +" is delivered upon count " + tracker.count());
         this.deliveryEvent = event;
+        tracker.stop();
     }
 
     public void send() {
 //        System.out.println(this + ": request is not yet completed, sending it to " + state.targetActorId);
         this.state.actor.caseSystem.gateway().inform(new RequestModelActor(creationEvent.command, state), state.actor.self());
+        tracker.start();
     }
 
     @Override
---
+++ engine/src/main/java/org/cafienne/actormodel/communication/request/state/RequestTracker.scala
@@ -0,0 +1,42 @@
+package org.cafienne.actormodel.communication.request.state
+
+import com.typesafe.scalalogging.LazyLogging
+import org.apache.pekko.actor.{Cancellable, Scheduler}
+import org.cafienne.system.CaseSystem
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.Duration
+
+class RequestTracker(val request: Request, val caseSystem: CaseSystem) extends Runnable with LazyLogging {
+  private implicit val dispatcher: ExecutionContext = caseSystem.system.dispatcher
+  private val scheduler: Scheduler = caseSystem.system.scheduler
+  private var retry: Option[Cancellable] = None
+  private val maxAttempts = 10
+  private var count = 0L
+
+  def run(): Unit = {
+    if (count > maxAttempts) {
+      logger.warn(s"Maximum number $maxAttempts of attempts to send request is reached. Timer-retry mechanism is canceled.")
+      stop()
+    } else {
+      logger.warn(s"Retrying (attempt $count) to send request $request to ${request.getCommand.getActorId} for which so far no delivery confirmation was received")
+      request.send()
+    }
+  }
+
+  def start(): Unit = {
+    if (count <= maxAttempts) {
+      stop() // First stop any current schedules
+      count += 1
+//      println("Increased count of " + request +"["+request.getCommand.getMessageId+"] to " + count)
+      // Slowly start delaying
+      retry = Some(scheduler.scheduleOnce(Duration.create(1 * count, TimeUnit.SECONDS), this))
+    }
+  }
+
+  def stop(): Unit = {
+//    println("Stopping tracker for " + request +"["+request.getCommand.getMessageId+"] on count " + count)
+    retry.foreach(_.cancel());
+  }
+}
