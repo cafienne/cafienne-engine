6660a09b8250f6f160a2eb8cdcf7bfc477d5bb87
Thijs Petter <thijspetter@gmail.com>
2025-03-15 16:58:22 +0100

    Upgrade various dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,9 +22,9 @@ object Dependencies {
   val pekkoHttpVersion    = "1.1.0"
   val pekkoVersion        = "1.1.2"
   val pekkoPersistenceVersion = "1.1.2"
-  val jacksonVersion     = "2.18.2"
+  val jacksonVersion     = "2.18.3"
   val enumeratumVersion  = "1.7.5"
-  val swaggerVersion     = "2.2.26"
+  val swaggerVersion     = "2.2.28"
   val slickVersion       = "3.5.2"
   val jasperVersion      = "6.20.0"
 
@@ -52,13 +52,13 @@ object Dependencies {
     , "com.datastax.oss"        %  "java-driver-core"                     % "4.17.0"
     , "com.datastax.oss"        %  "java-driver-query-builder"            % "4.17.0"
     // In-Memory persistence support
-    , "io.github.alstanchev"    %% "pekko-persistence-inmemory"            % "1.1.1"  excludeAll ExclusionRule(organization = "org.apache.pekko")
+    , "io.github.alstanchev"    %% "pekko-persistence-inmemory"            % "1.2.1"  excludeAll ExclusionRule(organization = "org.apache.pekko")
     // Config & logging
     , pekkoModule("slf4j")
     , "com.typesafe"            %  "config"                               % "1.4.3"
     , "com.typesafe.scala-logging" %% "scala-logging"                     % "3.9.5"
-    , "ch.qos.logback"          %  "logback-classic"                      % "1.5.12"
-    , "org.apache.commons"      %  "commons-text"                         % "1.12.0" // StrSubstitutor usage inside process tasks
+    , "ch.qos.logback"          %  "logback-classic"                      % "1.5.17"
+    , "org.apache.commons"      %  "commons-text"                         % "1.13.0" // StrSubstitutor usage inside process tasks
     , "com.beachape"            %% "enumeratum"                           % enumeratumVersion
     , "jakarta.xml.bind"        %  "jakarta.xml.bind-api"                 % "4.0.2" // Used in StringValue xsd date conversions
     // DB Schema
@@ -75,7 +75,7 @@ object Dependencies {
     , "com.fasterxml.jackson.module" %% "jackson-module-scala"            % jacksonVersion
     // Expression support (SPEL and JSONPath)
     , "com.jayway.jsonpath"     %  "json-path"                            % "2.9.0" // 2.7.0 is not compatible in expressions
-    , "org.springframework"     %  "spring-expression"                    % "5.3.23"
+    , "org.springframework"     %  "spring-expression"                    % "6.2.4"
   )
 
   //Extended engine deps are bundled with service
@@ -87,7 +87,7 @@ object Dependencies {
     , "com.lowagie"             %  "itext"                                % "2.1.7" // DO NOT CHANGE THIS VALUE
     // Mail & Calendar support
     , "com.sun.activation"      %  "jakarta.activation"                   % "2.0.1" // For mail & calendar support
-    , "jakarta.activation"      %  "jakarta.activation-api"               % "2.1.0" // For mail & calendar support
+    , "jakarta.activation"      %  "jakarta.activation-api"               % "2.1.3" // For mail & calendar support
     //, "jakarta.ws.rs"           %  "jakarta.ws.rs-api"                    % "3.1.0" // For mail & calendar support
     , "org.mnode.ical4j"        %  "ical4j"                               % "3.2.3"
     // Persistence support
@@ -111,12 +111,12 @@ object Dependencies {
     , "com.github.swagger-akka-http" %% "swagger-pekko-http"              % "2.12.2" excludeAll ExclusionRule(organization = "org.apache.pekko")
     , "com.github.swagger-akka-http" %% "swagger-scala-module"            % "2.12.3" excludeAll ExclusionRule(organization = "org.apache.pekko")
     , "javax.xml.bind"          %  "jaxb-api"                             % "2.3.1" // Note: this one is still needed for swagger-pekko-http
-    , "org.yaml"                %"snakeyaml"                              % "2.0"
+    , "org.yaml"                %"snakeyaml"                              % "2.4"
     // JWT Support
     , "com.github.t3hnar"       %% "scala-bcrypt"                         % "4.3.0"
     , "com.github.j5ik2o"       %% "sw4jj"                                % "1.1.60" // Simple scala Wrapper For Java-Jwt
-    , "com.nimbusds"            %  "nimbus-jose-jwt"                      % "9.47"
-    , "com.nimbusds"            %  "oauth2-oidc-sdk"                      % "11.20.1"
+    , "com.nimbusds"            %  "nimbus-jose-jwt"                      % "10.0.2"
+    , "com.nimbusds"            %  "oauth2-oidc-sdk"                      % "11.23.1"
   )
 
   val testEngine: Seq[ModuleID] = Seq(
